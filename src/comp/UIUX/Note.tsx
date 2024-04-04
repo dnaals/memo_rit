8a040800197ff2ba66aac838d1b12914775e90ae
dnaals <swm8793@naver.com>
2024-04-04 12:57:29 +0900

    폴더구조, css적용 연습

---
+++ src/comp/UIUX/Note.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../style/note.scss";
+function Note() {
+    return (
+        <div className='note'>
+            노트페이지입니다.
+        </div>
+    );
+}
+
+export default Note;
---
+++ src/comp/UIUX/Todolist.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../style/todo.scss";
+function Todolist() {
+    return (
+        <div className='todo'>
+            투두리스트 페이지 입니다.
+        </div>
+    );
+}
+
+export default Todolist;
